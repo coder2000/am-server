921e24352c2c1612e721de5a9b9cca02daf3b6d8
Dieter Lunn <coder2000@gmail.com>
2019-08-10 22:06:03 -0500

    feat(user): update user model

---
+++ src/entity/user.ts
@@ -0,0 +1,33 @@
+import {
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  emailAddress: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  googleId: string;
+
+  @Column()
+  facebookId: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
